85197453f50441dc9cdbb0ece6c99e8cf8291d6f
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-04-14 11:17:34 -0300

    ex 1, 2, 3

--- exe3/main.c
+++ exe3/main.c
@@ -11,6 +11,7 @@
 #include "mpu6050.h"
 
 const int I2C_CHIP_ADDRESS = 0x68;
+const int I2C_BMP280_ADDRESS = 0x76;  
 const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
 
@@ -21,8 +22,14 @@ void i2c_task(void *p) {
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
+    uint8_t buffer[1];
+    uint8_t reg_address = 0xD0; 
+
     // TODO
     // read id chip BMP280
+        // Lê o ID do BMP280
+    i2c_write_blocking(i2c_default, I2C_BMP280_ADDRESS, &reg_address, 1, true);
+    i2c_read_blocking(i2c_default, I2C_BMP280_ADDRESS, buffer, 1, false);
     printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
